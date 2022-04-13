ac3f450e107a31549216257ac0e0a9e7c4701a71
Malcolm Hendrix <melanna_camacho@Melannas-MacBook-Pro.local>
2022-04-13 15:31:49 -0700

    adding notes js and making final commit

---
+++ routes/notes.js
@@ -0,0 +1,61 @@
+const notes = require('express').Router();
+const { v4: uuidv4 } = require('uuid');
+
+const {
+    readFromFile,
+    ReadAndAppend,
+    writeToFile,
+} = require('../helpers/Utils.js');
+
+notes.get('/', (req, res) => {
+    readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
+});
+
+notes.get('/:id', (req, res) => {
+    const noteId = req.params.id;
+    readFromFile('./db/db.json')
+      .then((data) => JSON.parse(data))
+      .then((json) => {
+        const result = json.filter((note) => note.id === noteId);
+        return result.length > 0
+          ? res.json(result)
+          : res.json('No note with that ID');
+      });
+  });
+
+
+notes.delete('/:id', (req, res) => {
+    const noteId = req.params.id;
+    readFromFile('./db/db.json')
+        .then((data) => JSON.parse(data))
+        .then((json) => {
+        
+        const result = json.filter((note) => note.id !== noteId);
+
+        
+        writeToFile('./db/db.json', result);
+
+        res.json(`Note ${noteId} has been deleted 🗑️`);
+        });
+});
+
+
+notes.post('/', (req, res) => {
+
+    const { title, text } = req.body;
+    
+    if ( title && text ) {
+        const newNote = {
+            title,
+            text,
+            id: uuidv4(),
+        };
+        
+        readAndAppend(newNote, './db/db.json');
+        res.json('Note added successfully!');
+    } else { 
+        res.error("Error in adding note")
+    };
+});
+
+module.exports = notes;
