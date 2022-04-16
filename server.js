9485df2f7cec5a40c886b3270932a3d44a5540fa
Malcolm Hendrix <melanna_camacho@Melannas-MacBook-Pro.local>
2022-04-16 10:58:29 -0700

    fixing a few files

--- server.js
+++ server.js
@@ -1,23 +1,29 @@
-// adding the required modules 
+//adding the required modules 
 const path = require("path");
 const express = require("express");
-const { appendFile } = require("fs");
+const { clog } = require('./middleware/clog');
+const fs = require('fs')
+const api = require('./routes/index.js');
 
-// creating the server application
-const PORT = process.env.PORT || 3000;
+//creating the server application
+const PORT = process.env.PORT || 3001;
+const app = express();
 
-// read url
-app.use(express.urlencoded({extended: true}));
+//middleware clog
+app.use(clog);
+
+//middleware to parse json and urlencoded data
 app.use(express.json());
+app.use(express.urlencoded({ extended: true}));
+app.use('/api', api);
+app.use(express.static('public'));
 
-// js files
-require("./routes/apiRoutes")(app);
-require("./routes/htmlRoutes")(app);
-
-//use public folder
-app.use(express.static("public"));
-
-// add listener
-app.listen(PORT, function(){
-    console.log("App listening on PORT:" + PORT);
+//Get route for notes page
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public/notes.html'));
 });
+
+//add listener
+app.listen(PORT, () =>
+console.log(`listening at http://localhost:${PORT}`)
+);
